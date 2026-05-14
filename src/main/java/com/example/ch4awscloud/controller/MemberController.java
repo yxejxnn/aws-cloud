4506a42517b8efce76c5ae2698478d2f2c5e2b8c
정예진 <yxejxnn@gmail.com>
2026-05-14 15:13:23 +0900

    feat: MemberService, MemberController 추가

---
+++ src/main/java/com/example/ch4awscloud/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.example.ch4awscloud.controller;
+
+import com.example.ch4awscloud.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+}
---
+++ src/main/java/com/example/ch4awscloud/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.example.ch4awscloud.service;
+
+import com.example.ch4awscloud.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+}
