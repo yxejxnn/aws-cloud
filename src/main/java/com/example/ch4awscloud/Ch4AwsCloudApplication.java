41f6ad6fc45cc996d07c0aa06bf60acf824955fb
정예진 <yxejxnn@gmail.com>
2026-05-14 10:28:52 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/example/ch4awscloud/Ch4AwsCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch4awscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4AwsCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch4AwsCloudApplication.class, args);
+    }
+
+}
